f3d55d4313c0743faebdc86ad760cfe662f644aa
youssef <info@adambahri.com>
2026-04-25 17:30:21 +0100

    fix run script

--- run.bat
+++ run.bat
@@ -4,7 +4,4 @@ cd /d "C:\Users\youssef\projects\simple-websocket-app"
 :: Start WebSocket server in its own window
 start "🔌 WebSocket Server" cmd /k "node server.js"
 
-:: Start Static serve in its own window
-start "📦 Static Serve (5500)" cmd /k "npx serve . -p 5500"
-
-echo ✅ Both servers started successfully.
+echo ✅ server started successfully.
